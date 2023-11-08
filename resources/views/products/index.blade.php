dd50e7c10c3e0561485c8d2c893fe0439a757127
raimeiga <yusuke612@hotmail.com>
2023-11-08 14:55:46 +0900

    5.5 商品用のCRUDを作成/destroyアクション/商品を削除できるように、ビューを変更

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -15,10 +15,14 @@
          <td>{{ $product->price }}</td>
          <td>{{ $product->category_id }}</td>
          <td>
-             <a href="{{ route('products.show',$product->id) }}">Show</a>  <!-- 商品の個別ページ(show.blade.php)へのリンク -->
-             <a href="{{ route('products.edit',$product->id) }}">Edit</a>  <!-- 商品情報の編集ページ(edit.blade.php)へのリンク -->
-         </td>       
-         
+         <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+                <a href="{{ route('products.show',$product->id) }}">Show</a>  <!-- 商品の個別ページ(show.blade.php)へのリンク -->
+                <a href="{{ route('products.edit',$product->id) }}">Edit</a>  <!-- 商品情報の編集ページ(edit.blade.php)へのリンク -->
+              @csrf
+              @method('DELETE')  <!-- ←　リクエストがDELETEであると分かるように書き、数行上のactionでリクエストの送信先を指定 -->
+              <button type="submit">Delete</button>
+             </form>
+         </td>         
      </tr>
      @endforeach
  </table>
