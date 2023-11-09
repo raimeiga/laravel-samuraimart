c8c25ce3290bdfa34d353ee83116f78baa662bf7
raimeiga <yusuke612@hotmail.com>
2023-11-09 16:49:26 +0900

    13.3 サービスプロバイダを修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        //
+    {   // ←ページネーションの措置。自動的に「データベース内の商品の数 / 表示件数」で計算したページ数分のリンクを作成してくれる
+        Paginator::useBootstrap(); 
     }
 }
