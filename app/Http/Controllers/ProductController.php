fa1f5148642db6ca358ed2506663d393bf6f1013
raimeiga <yusuke612@hotmail.com>
2023-11-09 18:13:58 +0900

    14.2 カテゴリーで絞り込もう/コントローラーを修正

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,12 +15,17 @@ class ProductController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        /*↓ ココに書いてあったけど、消したくないので、コメントアウトして残しておく。
-            $products = Product::all(); すべての商品データをDBから取得して変数化し、下のcompact関数でindex.blade/phpに渡す
-        */
-        $products = Product::paginate(15);  // Productモデルのデータを15件ずつ、ページネーションで表示できる
+    public function index(Request $request)
+        {
+        if ($request->category !== null) {
+            $products = Product::where('category_id', $request->category)->paginate(15);
+            $total_count = Product::where('category_id', $request->category)->count();
+            $category = Category::find($request->category);
+        } else {
+            $products = Product::paginate(15);
+            $total_count = "";
+            $category = null;
+        }
          $categories = Category::all();     // 全てのカテゴリーを取得
          $major_category_names = Category::pluck('major_category_name')->unique(); 
                                 /* ↑ 全カテゴリーのデータからmajor_category_nameのカラムのみを取得.
