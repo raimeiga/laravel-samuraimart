954cce46350cd1a2ea5e3732812322fcd8d67926
raimeiga <yusuke612@hotmail.com>
2023-11-10 00:26:28 +0900

    15.2 ソート機能を追加しよう/モデルを修正

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,12 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Overtrue\LaravelFavorite\Traits\Favoriteable;   
 // ↑ お気に入りされるモデル（今回は商品のProduct）にuse Favoriteableとすることで、お気に入り機能を使えるようになる
-
-
+use Kyslik\ColumnSortable\Sortable;
+// ↑ Kyslik/column-sortable(というライブラリ)の使用宣言  Kyslik/column-sortableによりソート機能が使えるようになる
 class Product extends Model
 {
-    use HasFactory, Favoriteable; // ←お気に入りされるモデル（今回は商品のProduct）にuse Favoriteableとすることで、お気に入り機能を使えるようになる
-
+    use HasFactory, Favoriteable, Sortable; 
+    // ↑ useにFavoriteableを指定することで、Productモデルに、お気に入り機能を使えるようになる
+    // ↑ useにSortableを指定することで、Productモデルに、ソート機能を使えるようになる
     public function category()
     {
         return $this->belongsTo('App\Models\Category');
