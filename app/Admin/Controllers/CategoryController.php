f65eb18617bdacb7f4f7586429f0aa05e8f8adb4
raimeiga <yusuke612@hotmail.com>
2023-11-14 13:20:49 +0900

    5.6 カテゴリーの管理画面を変更しよう

--- app/Admin/Controllers/CategoryController.php
+++ app/Admin/Controllers/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Admin\Controllers;
 
 use App\Models\Category;
+use App\Models\MajorCategory;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
@@ -31,7 +32,7 @@ class CategoryController extends AdminController
         $grid->column('id', __('Id'))->sortable();  
         $grid->column('name', __('Name'));
         $grid->column('description', __('Description'));
-        $grid->column('major_category_name', __('Major category name'));
+        $grid->column('major_category_id', __('Major category name'))->editable('select', MajorCategory::all()->pluck('name', 'id'));
         $grid->column('created_at', __('Created at'))->sortable();  
         $grid->column('updated_at', __('Updated at'))->sortable(); 
 
@@ -39,7 +40,7 @@ class CategoryController extends AdminController
         $grid->filter(function($filter) {
             //$filter->like() = 部分一致のフィルタを追加する関数 第1引数にカラム名、第2引数に画面に表示する文字列を指定
             $filter->like('name', 'カテゴリー名');   
-            $filter->like('major_category_name', '親カテゴリー名'); 
+            $filter->in('major_category_id', '親カテゴリー名')->multipleSelect(MajorCategory::all()->pluck('name', 'id')); 
             // $filter->between()= 範囲指定のフィルタを追加する関数 datetime()を付与することで、カレンダーを表示して指定できるようになる
             $filter->between('created_at', '登録日')->datetime();  
         });                                                      
@@ -59,7 +60,7 @@ class CategoryController extends AdminController
         $show->field('id', __('Id'));
         $show->field('name', __('Name'));
         $show->field('description', __('Description'));
-        $show->field('major_category_name', __('Major category name'));
+        $show->field('major_category.name', __('Major category name'));
         $show->field('created_at', __('Created at'));
         $show->field('updated_at', __('Updated at'));
 
@@ -77,7 +78,7 @@ class CategoryController extends AdminController
 
         $form->text('name', __('Name'));
         $form->textarea('description', __('Description'));
-        $form->text('major_category_name', __('Major category name'));
+        $form->select('major_category_id', __('Major Category Name'))->options(MajorCategory::all()->pluck('name', 'id'));
 
         return $form;
     }
