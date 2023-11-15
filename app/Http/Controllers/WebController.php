9c83945587943ddcd70bb6c50d8e06870996f6bd
raimeiga <yusuke612@hotmail.com>
2023-11-15 14:11:25 +0900

    7.2 トップページにおすすめ商品を表示させよう/TOPページにおすすめの商品が表示されるよう編集

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -17,8 +17,11 @@ class WebController extends Controller
         $major_categories = MajorCategory::all();
         // 商品の登録日時（created_at）でソートして、新しい順に4つ取得し、下のcompactでビューに渡す
         $recently_products = Product::orderBy('created_at', 'desc')->take(4)->get();
-        
-        return view('web.index', compact('major_categories', 'categories', 'recently_products'));
+        // おすすめフラグがONの商品を3つ取得し、下のcompactでビューに渡す
+        $recommend_products = Product::where('recommend_flag', true)->take(3)->get();
+
+        return view('web.index', compact('major_categories', 'categories', 'recently_products', 'recommend_products'));
     }
 }
+
 
