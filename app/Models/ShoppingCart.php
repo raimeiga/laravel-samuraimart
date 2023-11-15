911cff8c2b36d0183e287b2c67b044dbcafa6397
raimeiga <yusuke612@hotmail.com>
2023-11-15 17:32:58 +0900

    9.3 売り上げの管理画面を追加/モデルにするテーブルにshoppingcartテーブルを指定

---
+++ app/Models/ShoppingCart.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShoppingCart extends Model
+{
+    use HasFactory;
+    // モデルにするテーブルにshoppingcartテーブルを指定
+    protected $table = 'shoppingcart';
+}
