0fb8796439c13607ee022a4d9c7f76f6339179ac
raimeiga <yusuke612@hotmail.com>
2023-11-16 14:17:20 +0900

    12.4 決済トークン用のカラムを追加時に障害となっていたファイルを小林講師に従い修正

--- database/migrations/2023_11_15_200934_drop_column_categories_table.php
+++ database/migrations/2023_11_15_200934_drop_column_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->dropColumn('major_category_name');
+            //$table->dropColumn('major_category_name');
         });
     }
 
